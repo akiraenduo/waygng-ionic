432ac796d3c62b82f5ff2b2a60f284069cfcb55d
Anthony Riefolo <ariefolo@maincare.fr>
2017-10-16 16:12:32 +0200

    update detail spot

--- src/pages/spot-detail/spot-detail.ts
+++ src/pages/spot-detail/spot-detail.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ModalController } from 'ionic-angular';
 import { SpotProvider } from '../../providers/spot/spotProvider';
 import { Observable } from 'rxjs/Observable';
 import { AuthProvider } from '../../providers/auth/auth';
+import { ModalLikePage } from '../modal-like/modal-like';
+import * as _ from 'lodash'
+
 
 /**
  * Generated class for the SpotDetailPage page.
@@ -18,14 +21,48 @@ import { AuthProvider } from '../../providers/auth/auth';
 export class SpotDetailPage {
 
   spot:Observable<any>;
+  userUid: any;
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
-              public spotProvider: SpotProvider) {
+              public auth: AuthProvider,
+              public spotProvider: SpotProvider,
+              public modalCtrl: ModalController) {
                 
 
               let spotKey = navParams.get("spotKey");
-              this.spot = spotProvider.getSpot(spotKey).valueChanges();
-              }
+
+              this.auth.user.subscribe(user => {
+                if (user) {
+                  this.userUid = user.uid;
+                  this.spot = spotProvider.getSpot(spotKey).valueChanges();
+                }
+              });
+
+    }
+
+
+  incrementLike(spot){
+    if(!spot["likes"]){
+      spot["likes"] = [];
+    }
+    const index = _.indexOf(spot["likes"], this.userUid);
+    if(index < 0){
+      spot.liked = true;
+      spot["likes"].push(this.userUid);
+      this.spotProvider.incrementLikes(spot.id,spot);
+    }else{
+      spot.liked = false; 
+      _.pullAt(spot["likes"], index);
+      this.spotProvider.incrementLikes(spot.id,spot);
+    }
+  }
+
+  openModalLike(spot) {
+    let myModal = this.modalCtrl.create(ModalLikePage, { 'usersUid': spot.likes });
+    myModal.present();
+  }
 
 }
+
+
