b9bc4317218302a1b2429e613a7e8540f61b7ded
Anthony Riefolo <ariefolo@maincare.fr>
2017-11-23 16:41:48 +0100

    ajout component login-button

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { MyLoaderComponent } from './my-loader/my-loader';
 import { IonicModule } from 'ionic-angular';
 import { MyHeaderLoaderComponent } from './my-header-loader/my-header-loader';
+import { LoginButtonComponent } from './login-button/login-button';
+import { TranslateModule } from '@ngx-translate/core';
 @NgModule({
 	declarations: [MyLoaderComponent,
-    MyHeaderLoaderComponent],
-	imports: [IonicModule],
+    MyHeaderLoaderComponent,
+    LoginButtonComponent],
+	imports: [IonicModule,TranslateModule.forChild(),],
 	exports: [MyLoaderComponent,
-    MyHeaderLoaderComponent]
+    MyHeaderLoaderComponent,
+    LoginButtonComponent],
+
 })
 export class ComponentsModule {}
--- src/pages/notification/notification.ts
+++ src/pages/notification/notification.ts
@@ -60,6 +60,7 @@ export class NotificationPage {
             this.loading = false});
       }else{
         this.notifications = null;
+        this.userUid = null;
       }
 
     }); 
